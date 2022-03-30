d98e370e3f4caa16ce450573f69ddcad2006792f
Brentley Jones <github@brentleyjones.com>
2022-03-30 15:49:53 -0500

    Improve error messages

--- tools/generator/src/Generator+SetTargetConfigurations.swift
+++ tools/generator/src/Generator+SetTargetConfigurations.swift
@@ -17,7 +17,7 @@ extension Generator {
         for (id, disambiguatedTarget) in disambiguatedTargets {
             guard let pbxTarget = pbxTargets[id] else {
                 throw PreconditionError(message: """
-Target "\(id)" not found in `pbxTargets`.
+Target "\(id)" not found in `pbxTargets`
 """)
             }
 
@@ -140,12 +140,12 @@ Target "\(id)" not found in `pbxTargets`.
                     let testHost = disambiguatedTargets[testHostID]?.target
                 else {
                     throw PreconditionError(message: """
-Test host target with id "\(testHostID)" not found
+Test host target with id "\(testHostID)" not found in `disambiguatedTargets`
 """)
                 }
                 guard let pbxTestHost = pbxTargets[testHostID] else {
                     throw PreconditionError(message: """
-Test host pbxTarget with id "\(testHostID)" not found
+Test host pbxTarget with id "\(testHostID)" not found in `pbxTargets`
 """)
                 }
 
@@ -300,7 +300,7 @@ extension Target {
             components[2] == "bin"
         else {
             throw PreconditionError(message: """
-packageBinDir is in unexpected format: \(packageBinDir)
+`packageBinDir` is in unexpected format: \(packageBinDir)
 """)
         }
         // Remove "bin/"
